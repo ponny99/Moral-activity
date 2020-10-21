888942615d39ed4eadda626b83a7660e86950634
Selfmade20e <tebogo1686@gmail.com>
2020-10-21 15:17:07 +0200

    uncommented the activities

--- test.js
+++ test.js
@@ -1,39 +1,44 @@
 // var named const and creat a list of students     
 
-// var names = [ 
-//     {name: "Jafta", mark :99}, 
-//     {name: "Boitumelo", mark :94}, 
-//     {name: "Tahidiso", mark :89}, 
-//     {name: "Thato", mark :79}, 
-//     {name: "Lesego", mark :90},
-// ] 
-// console.log(names)
+var names = [ 
+    {name: "Jafta", mark :99}, 
+    {name: "Boitumelo", mark :94}, 
+    {name: "Tahidiso", mark :89}, 
+    {name: "Thato", mark :79}, 
+    {name: "Lesego", mark :90},
+] 
+console.log(names)
 
-//  function getAverage(marks){
+//List the avarage mark of the marks
+
+ function getAverage(marks){
   
-//     var sumMarks = 0;
-//     for (var i=0; i==marks.lenghth-1; i++){
-//    sumMarks+=marks[i];
-//     }
+    var sumMarks = 0;
+    for (var i=0; i==marks.lenghth-1; i++){
+   sumMarks+=marks[i];
+    }
     
-//      var average = sumMarks / marks.length;
+     var average = sumMarks / marks.length;
     
-//       return Math.floor(average);
-//     }
+      return Math.floor(average);
+    }
 
+// List the highest mark 
+var a = [99, 94, 89, 79, 90];
 
-// var a = [99, 94, 89, 79, 90];
+console.log(eval(a.join('+'))/a.length)
 
-// console.log(eval(a.join('+'))/a.length)
+var array = [99, 94, 89, 79, 90];
+var largest= '';
 
-// var array = [99, 94, 89, 79, 90];
-// var largest= '';
+for (i=0; i<=largest;i++){
+    if (array>largest) {
+        var largest=array[i];
+    }
+}
 
-// for (i=0; i<=largest;i++){
-//     if (array>largest) {
-//         var largest=array[i];
-//     }
-// }
+ console.log(largest);
 
-// console.log(largest);
+ // List the highest and lowest mark
 
+ 
